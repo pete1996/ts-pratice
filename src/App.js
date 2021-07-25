7a7c11752b5ff9fb54c8d36a2c2935bc4f346a1e
yufeng03 <yufeng03@kuaishou.com>
2021-07-25 21:31:35 +0800

    srcss

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ export default class App extends React.Component{
         console.log('3');
         console.log('4');
         console.log('5');
+
+        console.log('6');
+
+
         clearInterval(this.timer)
     }
     render(){
